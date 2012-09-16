a6f8dd52c5808cca161f3d7531e0ae18a512673f
RJ Clay <jame@rocasa.us>
2012-09-16 12:49:13 -0400

    Bump version to 0.29

--- lib/FTN/JAM.pm
+++ lib/FTN/JAM.pm
@@ -15,11 +15,11 @@ FTN::JAM - A Perl extension for handleing JAM messagebases.
 
 =head1 VERSION
 
-Version 0.28
+Version 0.29
 
 =cut
 
-our $VERSION = '0.28';
+our $VERSION = '0.29';
 
 =head1 SYNOPSIS
 
