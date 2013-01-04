6783f1de37411703e3f33ff9d349ff15abba59c1
RJ Clay <jame@rocasa.us>
2013-01-04 14:27:40 -0500

    Update the copyright end year to 2013.

--- lib/FTN/JAM/Attr.pm
+++ lib/FTN/JAM/Attr.pm
@@ -113,7 +113,7 @@ can be found at L<https://bitbucket.org/johanbilling/jampm/overview>.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2010-2012 Robert James Clay, all rights reserved.
+Copyright 2010-2013 Robert James Clay, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
